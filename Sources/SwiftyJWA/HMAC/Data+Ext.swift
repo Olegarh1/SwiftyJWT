d9775f7548cc390e0c4651e84a0969ff602128ec
Zakladnyi <zakladnyio@gmail.com>
2025-07-23 12:46:24 +0300

    Add Data extension for bytes property

---
+++ Sources/SwiftyJWA/HMAC/Data+Ext.swift
@@ -0,0 +1,13 @@
+//
+// Data+Ext.swift
+// SwiftyJWT
+//
+// Created by Oleg Zakladnyi on 23.07.2025
+
+import Foundation
+
+extension Data {
+    var bytes: [UInt8] {
+        return [UInt8](self)
+    }
+}
